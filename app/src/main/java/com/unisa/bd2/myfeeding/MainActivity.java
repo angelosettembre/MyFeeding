cbb22fe3bbf75018472e7032c38ccc58cb8ec50f
Domenico <dtropeano@hotmail.it>
2018-06-07 14:31:39 +0200

    Navigation Drawer

--- app/src/main/java/com/unisa/bd2/myfeeding/MainActivity.java
+++ app/src/main/java/com/unisa/bd2/myfeeding/MainActivity.java
@@ -1,41 +1,82 @@
 package com.unisa.bd2.myfeeding;
 
-import android.support.v7.app.AppCompatActivity;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
 
-import com.mongodb.MongoClient;
-import com.mongodb.MongoClientURI;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoCursor;
-import com.mongodb.client.MongoDatabase;
-
-import org.bson.Document;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
 
-        MongoClient mongoClient = new MongoClient("34.207.149.93",27017);
 
-        System.out.println("COLLEGATO ");
+        mDrawerLayout = findViewById(R.id.drawer_layout);
 
-        MongoDatabase db = mongoClient.getDatabase("Food");
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        mDrawerLayout.closeDrawers();
 
-        MongoCollection<Document> collection = db.getCollection("ItalianFood");
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
 
-        MongoCursor<Document> cursor = collection.find().iterator();
-        try {
-            while (cursor.hasNext()) {
-                System.out.println("RECORD "+cursor.next().toJson());
+                        return true;
+                    }
+                });
+        mDrawerLayout.addDrawerListener(new DrawerLayout.DrawerListener() {
+            @Override
+            public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
+
             }
-        } finally {
-            cursor.close();
+
+            @Override
+            public void onDrawerOpened(@NonNull View drawerView) {
+
+            }
+
+            @Override
+            public void onDrawerClosed(@NonNull View drawerView) {
+
+            }
+
+            @Override
+            public void onDrawerStateChanged(int newState) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
         }
-
-
+        return super.onOptionsItemSelected(item);
     }
 }
